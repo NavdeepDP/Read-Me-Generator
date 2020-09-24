2da8d981a75348908798d4439313fa95adfe5cd8
NP <navdeep.puri@gmail.com>
2020-09-24 07:35:06 -0400

    added code to get the user input and write to the readme file

--- index.js
+++ index.js
@@ -1,16 +1,98 @@
+
+var inquirer = require("inquirer");
+var fs = require("fs");
+var gMarkDown = require("./utils/generateMarkdown.js");
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "Title of the project: ",
+        name: "title"
 
+    },
+    {
+        type: "input",
+        message: "Project Description: ",
+        name: "description"
+
+    },
+    {
+        type: "input",
+        message: "Installation Steps: ",
+        name: "installation"
+
+    },
+    {
+        type: "input",
+        message: "Usage Information: ",
+        name: "usage"
+
+    },
+    {
+        type: "list",
+        message: "License:",
+        choices: ["Apache 2.0",
+            "BSD 3",
+            "MIT",
+            "GNU GPL v3",
+            "Mozilla Public License 2.0",
+            "The zlib/libpng License",
+            "None"
+        ],
+        name: "licence"
+
+    },
+    {
+        type: "input",
+        message: "Contribution Guideline: ",
+        name: "contribution"
+
+    },
+    {
+        type: "input",
+        message: "Test Instructions: ",
+        name: "test"
+
+    },
+    {
+        type: "input",
+        message: "GitHub Username: ",
+        name: "username"
+
+    },
+    {
+        type: "input",
+        message: "Email: ",
+        name: "email"
+
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+
+        if (err)
+            throw err;
+    });
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then(function (response) {
+        console.log(response);
+
+        const markDownResponse = gMarkDown.generateMarkdownFunciton(response);
+
+        writeToFile("README-Generated.md", markDownResponse);
+
+    }).catch(function (err) {
+        throw err;
+    });
 
 }
 
 // function call to initialize program
 init();
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,46 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+
+## Table of contents
+* [Title](#Title)
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.test}
+
+## Questions
+Contact at:
+
+Email: ${data.email}
+
+Github: [GitHub Link](https://github.com/${data.username})
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+generateMarkdownFunciton:  generateMarkdown
+};
